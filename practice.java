a2af7a243f1ebdc616f52947408d70adecb1cf05
foreducation794 <foreducation794@gmail.com>
2025-08-01 14:03:02 +0530

    basic loop done

--- practice.java
+++ practice.java
@@ -6,5 +6,29 @@ public class practice {
 
         int a = sc.nextInt();
 
+        // for (int i = 1; i <= 10; i++) {
+        // System.out.print(a * i + " ");
+        // }
+
+        // int factorial = 1;
+        // // for (int i = 1; i <= a; i++) {
+        // // factorial = factorial * i;
+        // // }
+        // while (a > 0) {
+        // int dec = a--;
+        // factorial = factorial * dec;
+        // }
+        // System.out.println(factorial);
+
+        // int sum = 0;
+        // // for (int i = 1; i <= a; i++) {
+        // // sum = sum + i;
+        // // }
+        // while (a > 0) {
+        // int num = a--;
+        // sum = sum + num;
+        // }
+        // System.out.println(sum);
+
     }
 }
