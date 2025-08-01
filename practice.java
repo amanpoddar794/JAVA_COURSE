537c6bad6fd58ab2cfb468a270d388d46349c643
foreducation794 <amanrocking7654@gmail.com>
2025-08-02 00:54:51 +0530

    Hard Level Loop Done

--- practice.java
+++ practice.java
@@ -88,17 +88,91 @@ public class practice {
         // System.out.println();
         // }
 
-        for (int i = 1; i <= a; i++) {
-            for (int sp = a - i; sp > 0; sp--) {
-                System.out.print(" ");
-            }
-            int num = 1;
-            for (int j = 0; j < i; j++) {
-                System.out.print(num + " ");
-                num = num * (i - 1 - j) / (j + 1);
-            }
-            System.out.println();
-        }
+        // for (int i = 1; i <= a; i++) {
+        // for (int sp = a - i; sp > 0; sp--) {
+        // System.out.print(" ");
+        // }
+        // int num = 1;
+        // for (int j = 0; j < i; j++) {
+        // System.out.print(num + " ");
+        // num = num * (i - 1 - j) / (j + 1);
+        // }
+        // System.out.println();
+        // }
 
+        // for (int i = 1; i <= a; i++) {
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print("*");
+        // }
+        // for (int j = 1; j <= 2 * (a - i); j++) {
+        // System.out.print(" ");
+        // }
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print("*");
+        // }
+        // System.out.println();
+        // }
+        // for (int i = a - 1; i >= 1; i--) {
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print("*");
+        // }
+        // for (int j = 1; j <= 2 * (a - i); j++) {
+        // System.out.print(" ");
+        // }
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print("*");
+        // }
+        // System.out.println();
+        // }
+
+        // int orignal = a;
+        // int num = 0;
+        // int add = 0;
+        // while (a > 0) {
+        // int lastDigit = a % 10;
+        // a = a / 10;
+        // num = lastDigit * lastDigit * lastDigit;
+        // add = num + add;
+        // }
+        // if (orignal == add) {
+        // System.out.println("Armstrong");
+        // } else {
+        // System.out.println("Not Armstrong");
+        // }
+
+        // for (int i = 1; i <= a; i++) {
+        // for (int j = a - i; j >= 1; j--) {
+        // System.out.print(" ");
+        // }
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print("*");
+        // }
+        // for (int j = 1; j < i; j++) {
+        // System.out.print("*");
+        // }
+        // System.out.println();
+        // }
+        // for (int i = a - 1; i >= 1; i--) {
+        // for (int j = a - i; j >= 1; j--) {
+        // System.out.print(" ");
+        // }
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print("*");
+        // }
+        // for (int j = 1; j < i; j++) {
+        // System.out.print("*");
+        // }
+        // System.out.println();
+        // }
+
+        // int b = sc.nextInt();
+        // int gcd = 1;
+        // Math.min(a, b);
+        // for (int i = 1; i < +Math.min(a, b); i++) {
+        // if (a % i == 0 && b % i == 0) {
+        // gcd = i;
+        // }
+        // }
+        // System.out.println(gcd + " is the GCD of " + a + " " + b);
     }
 }
