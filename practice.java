26bdced450cf8abd6b3629416618cea8ac8d21ec
foreducation794 <amanrocking7654@gmail.com>
2025-08-02 21:46:38 +0530

    Function & method basic done

--- practice.java
+++ practice.java
@@ -1,178 +1,64 @@
 import java.util.*;
 
 public class practice {
+
+    // public static void printName(String name) {
+    // System.out.println("Hello, " + name + "!");
+    // }
+
+    // public static int printSquare(int n) {
+    // // int square = n * n;
+    // // System.out.println(square);
+    // return n * n;
+    // }
+
+    // public static void numberCheck(int n) {
+    // if (n % 2 == 0) {
+    // System.out.println("Even");
+    // } else {
+    // System.out.println("Odd");
+    // }
+    // }
+
+    // public static void printTable(int n) {
+    // for (int i = 1; i <= 10; i++) {
+    // System.out.println(n + " x " + i + " = " + n * i);
+    // }
+    // }
+
+    // public static int findMin(int a, int b, int c) {
+    // int min;
+    // if (a < b && a < c) {
+    // min = a;
+    // System.out.println(min + " is minimum");
+    // } else if (b < a && b < c) {
+    // min = b;
+    // System.out.println(min + " is minimum");
+    // } else {
+    // min = c;
+    // System.out.println(min + " is minimum");
+    // }
+    // return min;
+    // }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int a = sc.nextInt();
+        // String name = sc.nextLine();
+        // printName(name);
 
-        // for (int i = 1; i <= 10; i++) {
-        // System.out.print(a * i + " ");
-        // }
+        // int n = sc.nextInt();
+        // System.out.println(printSquare(n));
 
-        // // int factorial = 1;
-        // // for (int i = 1; i <= a; i++) {
-        // // factorial = factorial * i;
-        // // }
-        // int factorial = 1;
-        // while (a > 0) {
-        // int dec = a--;
-        // factorial = factorial * dec;
-        // }
-        // System.out.println(factorial);
+        // int n = sc.nextInt();
+        // numberCheck(n);
 
-        // // int sum = 0;
-        // // for (int i = 1; i <= a; i++) {
-        // // sum = sum + i;
-        // // }
-        // while (a > 0) {
-        // int num = a--;
-        // sum = sum + num;
-        // }
-        // System.out.println(sum);
+        // int n = sc.nextInt();
+        // printTable(n);
 
-        // // int orignal = a;
-        // // int rev = 0;
-        // // while (a > 0) {
-        // // int lastDigit = a % 10;
-        // // a = a / 10;
-        // // rev = rev * 10 + lastDigit;
-        // // }
-        // do {
-        // int lastDigit = a % 10;
-        // a = a / 10;
-        // rev = rev * 10 + lastDigit;
-        // } while (a > 0);
-        // System.out.println(rev);
-
-        // int orignal = a;
-        // int count = 0;
-        // a = Math.abs(a);
-        // if (a == 0) {
-        // count = 1;
-        // } else {
-        // while (a > 0) {
-        // int lastDigit = a % 10;
-        // a = a / 10;
-        // count++;
-        // }
-        // }
-        // System.out.println(count);
-
-        // int orignal = a;
-        // int rev = 0;
-        // while (a > 0) {
-        // int lastDigit = a % 10;
-        // a = a / 10;
-        // rev = rev * 10 + lastDigit;
-        // }
-        // if (orignal == rev) {
-        // System.out.println("Palindrome");
-        // } else {
-        // System.out.println("Not Palindrome");
-        // }
-
-        // for (int i = 1; i <= a; i++) {
-        // for (int j = 1; j <= i; j++) {
-        // System.out.print("*");
-        // }
-        // System.out.println();
-        // }
-
-        // int num = 1;
-        // for (int i = 1; i <= a; i++) {
-        // for (int j = 1; j <= i; j++) {
-        // System.out.print(num + " ");
-        // num++;
-        // }
-        // System.out.println();
-        // }
-
-        // for (int i = 1; i <= a; i++) {
-        // for (int sp = a - i; sp > 0; sp--) {
-        // System.out.print(" ");
-        // }
-        // int num = 1;
-        // for (int j = 0; j < i; j++) {
-        // System.out.print(num + " ");
-        // num = num * (i - 1 - j) / (j + 1);
-        // }
-        // System.out.println();
-        // }
-
-        // for (int i = 1; i <= a; i++) {
-        // for (int j = 1; j <= i; j++) {
-        // System.out.print("*");
-        // }
-        // for (int j = 1; j <= 2 * (a - i); j++) {
-        // System.out.print(" ");
-        // }
-        // for (int j = 1; j <= i; j++) {
-        // System.out.print("*");
-        // }
-        // System.out.println();
-        // }
-        // for (int i = a - 1; i >= 1; i--) {
-        // for (int j = 1; j <= i; j++) {
-        // System.out.print("*");
-        // }
-        // for (int j = 1; j <= 2 * (a - i); j++) {
-        // System.out.print(" ");
-        // }
-        // for (int j = 1; j <= i; j++) {
-        // System.out.print("*");
-        // }
-        // System.out.println();
-        // }
-
-        // int orignal = a;
-        // int num = 0;
-        // int add = 0;
-        // while (a > 0) {
-        // int lastDigit = a % 10;
-        // a = a / 10;
-        // num = lastDigit * lastDigit * lastDigit;
-        // add = num + add;
-        // }
-        // if (orignal == add) {
-        // System.out.println("Armstrong");
-        // } else {
-        // System.out.println("Not Armstrong");
-        // }
-
-        // for (int i = 1; i <= a; i++) {
-        // for (int j = a - i; j >= 1; j--) {
-        // System.out.print(" ");
-        // }
-        // for (int j = 1; j <= i; j++) {
-        // System.out.print("*");
-        // }
-        // for (int j = 1; j < i; j++) {
-        // System.out.print("*");
-        // }
-        // System.out.println();
-        // }
-        // for (int i = a - 1; i >= 1; i--) {
-        // for (int j = a - i; j >= 1; j--) {
-        // System.out.print(" ");
-        // }
-        // for (int j = 1; j <= i; j++) {
-        // System.out.print("*");
-        // }
-        // for (int j = 1; j < i; j++) {
-        // System.out.print("*");
-        // }
-        // System.out.println();
-        // }
-
+        // int a = sc.nextInt();
         // int b = sc.nextInt();
-        // int gcd = 1;
-        // Math.min(a, b);
-        // for (int i = 1; i < +Math.min(a, b); i++) {
-        // if (a % i == 0 && b % i == 0) {
-        // gcd = i;
-        // }
-        // }
-        // System.out.println(gcd + " is the GCD of " + a + " " + b);
+        // int c = sc.nextInt();
+        // findMin(a, b, c);
     }
 }
