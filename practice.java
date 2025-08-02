09aac3847fca4685baa161c9aa065f8e98df0028
foreducation794 <amanrocking7654@gmail.com>
2025-08-02 22:35:46 +0530

    Function & method medium done

--- practice.java
+++ practice.java
@@ -41,6 +41,69 @@ public class practice {
     // return min;
     // }
 
+    // public static boolean isPrime(int n) {
+    // boolean isPrime = true;
+    // if (n <= 1)
+    // return false;
+    // for (int i = 2; i <= n / 2; i++) {
+    // if (n % i == 0) {
+    // return false;
+    // }
+    // }
+    // return true;
+    // }
+
+    // public static int factorial(int n) {
+    // int factorial = 1;
+    // for (int i = 1; i <= n; i++) {
+    // factorial = factorial * i;
+    // }
+    // return factorial;
+    // }
+
+    // public static int reverse(int n) {
+    // int orignal = n;
+    // int rev = 0;
+    // while (n > 0) {
+    // int last = n % 10;
+    // n = n / 10;
+    // rev = rev * 10 + last;
+    // }
+    // return rev;
+    // }
+
+    // public static int palindrome(int n) {
+    // int orignal = n;
+    // int palindrome = 0;
+    // while (n > 0) {
+    // int last = n % 10;
+    // n = n / 10;
+    // palindrome = palindrome * 10 + last;
+    // }
+    // if (orignal == palindrome) {
+    // System.out.println(orignal + " is Palindrome");
+    // } else {
+    // System.out.println(orignal + " is not Palindrome");
+    // }
+    // return orignal;
+    // }
+
+    // public static int digitCount(int n) {
+    // int orignal = n;
+    // int count = 0;
+    // n = Math.abs(n);
+    // if (n == 0) {
+    // count = 1;
+    // } else {
+    // while (n > 0) {
+    // int last = n % 10;
+    // n = n / 10;
+    // count++;
+    // }
+    // }
+    // return count;
+    // }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -60,5 +123,24 @@ public class practice {
         // int b = sc.nextInt();
         // int c = sc.nextInt();
         // findMin(a, b, c);
+
+        // int n = sc.nextInt();
+        // if (isPrime(n)) {
+        // System.out.println(n + " is prime");
+        // } else {
+        // System.out.println(n + " is not prime");
+        // }
+
+        // int n = sc.nextInt();
+        // System.out.println(factorial(n));
+
+        // int n = sc.nextInt();
+        // System.out.println(reverse(n));
+
+        // int n = sc.nextInt();
+        // palindrome(n);
+
+        // int n = sc.nextInt();
+        // System.out.println(digitCount(n));
     }
 }
